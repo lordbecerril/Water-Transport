d888f94866f718e9374910f6690e69f8f33958bd
lordbecerril <becerrilblas@gmail.com>
2020-09-07 19:19:38 -0700

    Fixed all the graphs

--- dataplotter.py
+++ dataplotter.py
@@ -2,6 +2,7 @@
     AUTHOR: Eric Becerril-Blas
 '''
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import *
 import pandas as pd
 from pandas import *
 
@@ -44,34 +45,75 @@ def create_dataframe(f_name):
     #print(df)
 
 
-def plot_data(df, title, savename):
+def line_graph(df, title, savename, color):
     '''
         PURPOSE:
             The following function creates the line graph
     '''
-    df.plot(x ='Time (years)', y='e', kind = 'line', title = title)
-    plt.savefig(savename)
+    fig, ax = subplots()
 
+    df.plot(x ='Time (years)', y='e', kind = 'line', title = title, color = color, ax=ax)
+    ax.legend(["Eccentricity"])
+    ax.set_ylabel("Eccentricity")
+    plt.tight_layout()
+
+    plt.savefig(savename)
+    plt.clf()
+
+
+def plot_together(df1, df2, savename):
+    fig, ax = subplots()
+
+    ax = df1.plot(x ='Time (years)', y='e', kind = 'line', color = 'blue', title = "Earth and Jupiter")
+    df2.plot(ax=ax, x ='Time (years)', y='e', kind = 'line', color = 'red')
+    ax.legend(["Earth Sized Planet e", "Jupiter Sized Planet e"])
+    ax.set_ylabel("Eccentricity")
+    plt.tight_layout()
+
+    plt.savefig(savename)
+    plt.clf()
+
+def dataplotter(earthmoo, jupiter, earthplot, jupiterplot, togetherplot):
+    print("EARTHMOO.aei:")
+    earthmoo_df = create_dataframe(earthmoo)
+    print(" ")
+
+    print("JUPITERMOO.aei")
+    jupiter_df = create_dataframe(jupiter)
+    print(" ")
+
+    # Plot EARTHMOO
+    line_graph(earthmoo_df, "Earth Sized Planet", earthplot, "blue")
+
+    # Plot JUPITER
+    line_graph(jupiter_df, "Jupiter Sized Planet", jupiterplot, "red")
+
+    # plot them together
+    plot_together(earthmoo_df, jupiter_df, togetherplot)
 
 
 def main():
     print("Hello World From dataplotter.py ")
+
     # Handling run1 data
     print("Plotting data from run1----------------------")
-
-    print("EARTHMOO.aei:")
-    earthmoo_df = create_dataframe('./run1/EARTHMOO.aei')
+    dataplotter('./run1/EARTHMOO.aei','./run1/JUPITER.aei',"./run1/earthmoo.eps","./run1/jupiter.eps","./run1/together.eps")
     print(" ")
 
-    print("JUPITERMOO.aei")
-    jupiter_df = create_dataframe('./run1/JUPITER.aei')
+    # Handling run2 data
+    print("Plotting data from run2----------------------")
+    dataplotter('./run2/EARTHMOO.aei','./run2/JUPITER.aei',"./run2/earthmoo.eps","./run2/jupiter.eps","./run2/together.eps")
     print(" ")
 
-    # Plot EARTHMOO
-    plot_data(earthmoo_df,"Earth Sized Planet", "./run1/earthmoo.eps")
+    # Handling run3 data
+    print("Plotting data from run3----------------------")
+    dataplotter('./run3/EARTHMOO.aei','./run3/JUPITER.aei',"./run3/earthmoo.eps","./run3/jupiter.eps","./run3/together.eps")
+    print(" ")
 
-    # Plot JUPITER
-
+    # Handling run4 data
+    print("Plotting data from run4----------------------")
+    dataplotter('./run4/EARTHMOO.aei','./run4/JUPITER.aei',"./run4/earthmoo.eps","./run4/jupiter.eps","./run4/together.eps")
+    print(" ")
 
 
 if __name__== "__main__":
