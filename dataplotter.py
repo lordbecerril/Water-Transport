253526136648a4c7b4698896859673e31cca354f
lordbecerril <becerrilblas@gmail.com>
2020-10-20 11:09:28 -0700

    New Runs added

--- dataplotter.py
+++ dataplotter.py
@@ -122,7 +122,6 @@ def main():
     dataplotter('./run_6/EARTHMOO.aei','./run_6/JUPITER.aei',"./run_6/earthmoo.png","./run_6/jupiter.png","./run_6/together.png")
     print(" ")
 
-    '''
     # Handling run7 data
     print("Plotting data from run7----------------------")
     dataplotter('./run_7/EARTHMOO.aei','./run_7/JUPITER.aei',"./run_7/earthmoo.png","./run_7/jupiter.png","./run_7/together.png")
@@ -133,29 +132,71 @@ def main():
     dataplotter('./run_8/EARTHMOO.aei','./run_8/JUPITER.aei',"./run_8/earthmoo.png","./run_8/jupiter.png","./run_8/together.png")
     print(" ")
 
+    # Handling run9 data
+    print("Plotting data from run9----------------------")
+    dataplotter('./run_9/EARTHMOO.aei','./run_9/JUPITER.aei',"./run_9/earthmoo.png","./run_9/jupiter.png","./run_9/together.png")
+    print(" ")
+
+    # Handling run10 data
+    print("Plotting data from run10----------------------")
+    dataplotter('./run_10/EARTHMOO.aei','./run_10/JUPITER.aei',"./run_10/earthmoo.png","./run_10/jupiter.png","./run_10/together.png")
+    print(" ")
     '''
+
+
+    # jupiter Eccentricity changes
+    run0_df = create_dataframe("./run_5/EARTHMOO.aei")
     run1_df = create_dataframe("./run1/EARTHMOO.aei")
-    run2_df = create_dataframe("./run2/EARTHMOO.aei")
-    run3_df = create_dataframe("./run3/EARTHMOO.aei")
-    run4_df = create_dataframe("./run4/EARTHMOO.aei")
+    run2_df = create_dataframe("./run_6/EARTHMOO.aei")
+    run3_df = create_dataframe("./run2/EARTHMOO.aei")
+    run4_df = create_dataframe("./run_9/EARTHMOO.aei")
+    run5_df = create_dataframe("./run_7/EARTHMOO.aei")
 
     #starting graph
-    fig, ax = subplots()
+    fig, ax = plt.subplots(figsize=(7,2))
 
-    ax = run1_df.plot(x ='Time (years)', y='e', kind = 'line', color = 'blue', title = "Earth Sized Planets Eccentricities")
-    run2_df.plot(ax=ax, x ='Time (years)', y='e', kind = 'line', color = 'red')
-    run3_df.plot(ax=ax, x ='Time (years)', y='e', kind = 'line', color = 'green')
-    run4_df.plot(ax=ax, x ='Time (years)', y='e', kind = 'line', color = 'yellow')
+    ax = run0_df.plot(x ='Time (years)', y='e', kind = 'line', color = 'blue', title = "Earth Sized Planets Eccentricities Based on Jupiter Eccentricities")
+    run1_df.plot(ax=ax, x ='Time (years)', y='e', kind = 'line', color = 'red')
+    run2_df.plot(ax=ax, x ='Time (years)', y='e', kind = 'line', color = 'green')
+    run3_df.plot(ax=ax, x ='Time (years)', y='e', kind = 'line', color = 'yellow')
+    run4_df.plot(ax=ax, x ='Time (years)', y='e', kind = 'line', color = 'm')
+    run5_df.plot(ax=ax, x ='Time (years)', y='e', kind = 'line', color = 'k')
 
 
 
-    ax.legend(["run1 e", "run2 e", "run3 e","run4 e"])
+    ax.legend(["$e_j$ = 0.0","$e_j$ = 0.1", "$e_j$ = 0.2", "$e_j$ = 0.3","$e_j$ = 0.4","$e_j$ = 0.5"],bbox_to_anchor=(1.05, 1))#,title='Jupiter Eccentricities'
     ax.set_ylabel("Eccentricity")
     plt.tight_layout()
     #plt.show()
     plt.savefig("all_Earth_e.png")
     plt.clf()
-    '''
+
+
+    # jupiter inclination changes
+    run0_df = create_dataframe("./run_5/EARTHMOO.aei")
+    run1_df = create_dataframe("./run_10/EARTHMOO.aei")
+    run2_df = create_dataframe("./run3/EARTHMOO.aei")
+    run3_df = create_dataframe("./run_8/EARTHMOO.aei")
+    run4_df = create_dataframe("./run4/EARTHMOO.aei")
+
+    #starting graph
+    fig, ax = subplots()
+
+    ax = run0_df.plot(x ='Time (years)', y='e', kind = 'line', color = 'blue', title = "Earth Sized Planets Eccentricities Based on Jupiter Inclinations")
+    run1_df.plot(ax=ax, x ='Time (years)', y='e', kind = 'line', color = 'red')
+    run2_df.plot(ax=ax, x ='Time (years)', y='e', kind = 'line', color = 'green')
+    run3_df.plot(ax=ax, x ='Time (years)', y='e', kind = 'line', color = 'yellow')
+    run4_df.plot(ax=ax, x ='Time (years)', y='e', kind = 'line', color = 'm')
+
+
+
+    ax.legend(["$i_j$ = 0","$i_j$ = 15", "$i_j$ = 30", "$i_j$ = 45","$i_j$ = 60"],bbox_to_anchor=(1.05, 1))#title='Jupiter Inclinations'
+    ax.set_ylabel("Inclination of Earth Sized Planet")
+    plt.tight_layout()
+    #plt.show()
+    plt.savefig("all_Earth_e_i.png")
+    plt.clf()
+
 
 if __name__== "__main__":
     main()
